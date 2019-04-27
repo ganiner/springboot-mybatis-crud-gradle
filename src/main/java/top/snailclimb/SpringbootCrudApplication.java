6c932bdb0cdc3d2b90ed437c8fda2236caf66b2f
SnailClimb <koushuangbwcx@163.com>
2019-04-27 11:19:36 +0800

    Update SpringbootCrudApplication.java

--- src/main/java/top/snailclimb/SpringbootCrudApplication.java
+++ src/main/java/top/snailclimb/SpringbootCrudApplication.java
@@ -3,6 +3,8 @@ package top.snailclimb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class SpringbootCrudApplication {
 
